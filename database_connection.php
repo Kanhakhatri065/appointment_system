27adcbdd44f02fdf8eebac3df27b5a26a3151db4
DESKTOP-F52MIK9\Varun <peddivarunkumar99@gmail.com>
2020-10-28 16:43:33 +0530

    takes_care_extra_'now

--- database_connection.php
+++ database_connection.php
@@ -3,5 +3,11 @@
     $username = "root";
     $password = "";
    $conn = mysqli_connect("$servername", "$username", "", "appointment_system")or die($mysqli_error($con))
-   
+   //database name has been set to appointment_system and the access method has been changed
+   //it was 
+   // $conn =  new mysqli($servername, $username, $password);
+
+   // if($conn->connect_error) {
+    //    die("Connection failed : " . $conn->connect_error);
+    //}
 ?>
