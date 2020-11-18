e885c823a6bdc08e772fe146522d01a2d4ae47db
DESKTOP-F52MIK9\Varun <peddivarunkumar99@gmail.com>
2020-11-18 11:01:06 +0530

    patientlogin

--- login_process_patient.php
+++ login_process_patient.php
@@ -1,1 +1,20 @@
+<?php session_start()?>
 <?php
+include("database_connection.php");
+$email_id=$_POST['email_id'];
+$password=$_POST['password'];
+$select_query="select * from patients where patient_email='$email_id' and patient_password='$password'";
+$select_query_result=mysqli_query($conn,$select_query) or die(mysqli_error($conn));
+if(mysqli_num_rows($select_query_result)==1){
+   
+    $row=mysqli_fetch_array($select_query_result);
+    $_SESSION['doctor_id']=$row['doctor_id'];
+    $_SESSION['doctor_email']=$row['doctor_email'];
+   
+    header('location:index.php');
+    
+}
+else{
+    header('location:logindoctor.php');
+}
+?>
